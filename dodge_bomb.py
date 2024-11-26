f9cb3596c414e06c154f8764785344a9c9b428f5
C0A23085 <c0a23085b6@edu.teu.ac.jp>
2024-11-26 16:53:08 +0900

    演習課題１

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -2,6 +2,7 @@ import os
 import random
 import sys
 import pygame as pg
+import time
 
 
 WIDTH, HEIGHT = 1100, 650
@@ -13,11 +14,11 @@ DELTA = {
 }
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-
-#引数で与えられたRectが画面内に収まっているかどうかを判定する関数
-#引数：こうかとんRect or 爆弾Rect
-#戻り値：True(画面内に収まっている) or False(画面外に出ている)
-
+"""
+引数で与えられたRectが画面内に収まっているかどうかを判定する関数
+引数：こうかとんRect or 爆弾Rect
+戻り値：True(画面内に収まっている) or False(画面外に出ている)
+"""
 
 def check_bound(rct):
     yoko, tate = True, True
@@ -26,6 +27,40 @@ def check_bound(rct):
     if rct.top < 0 or HEIGHT < rct.bottom:
         tate = False
     return yoko, tate
+
+def game_over(screen):
+    """
+    ゲームオーバー画面を表示する。
+    画面をブラックアウトし、「Game Over」を表示し、
+    泣いているこうかとん画像を貼り付ける。
+
+    Args:
+        screen (pg.Surface): ゲーム画面Surface。
+        kk_rct (pg.Rect): こうかとんの位置情報Rect。
+    """
+    # 半透明の黒い背景を作成
+    black_screen = pg.Surface((WIDTH, HEIGHT))
+    pg.draw.rect(black_screen, (0, 0, 0), (0, 0, WIDTH, HEIGHT))  # RGBA: 半透明
+    black_screen.set_alpha(200)  # 透過度を設定
+    screen.blit(black_screen, (0, 0)) # ブラックアウト画面の描画
+
+    # 「Game Over」の文字を作成
+    font = pg.font.Font(None, 80)  # フォントサイズ80
+    text = font.render("Game Over", True, (255, 255, 255))  # 白文字
+    screen.blit(text, (WIDTH // 2 - 150, HEIGHT // 2 - 25 ))  # 画面中央に配置
+
+    # 泣いているこうかとん画像の描画
+    kk_crying_img = pg.transform.rotozoom(pg.image.load("fig/8.png"), 0, 0.9)
+    kk_crying_rct = kk_crying_img.get_rect()
+    kk_crying_rct.center = WIDTH // 2 + 200, HEIGHT // 2 + 2
+    screen.blit(kk_crying_img, kk_crying_rct)
+    kk_crying_rct.center = WIDTH // 2 - 200, HEIGHT // 2 
+    screen.blit(kk_crying_img, kk_crying_rct)
+
+    # 画面を更新して5秒間停止
+    pg.display.update()
+    time.sleep(5)
+    return
 
 
 def main():
@@ -37,6 +72,7 @@ def main():
     kk_rct.center = 300, 200
     bb_img = pg.Surface((20, 20)) #爆弾用の空surface
     pg.draw.circle(bb_img, (255, 0, 0), (10,10), 10) #爆弾円を描く
+    bb_img.set_colorkey(0, 0)
     bb_rct = bb_img.get_rect()
     bb_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
     vx, vy = 5, 5
@@ -47,6 +83,7 @@ def main():
             if event.type == pg.QUIT: 
                 return
         if kk_rct.colliderect(bb_rct):
+            game_over(screen)
             break #爆弾に当たったらゲームオーバー
         screen.blit(bg_img, [0, 0]) 
 
